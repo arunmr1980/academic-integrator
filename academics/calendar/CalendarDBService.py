cb541f83985b4b41028a6f8807c4a99b60282da6
muhammed fanish <fanishmuhammed@gmail.com>
2020-08-25 10:58:21 +0530

    scan changed to query in calendardbservice

--- academics/calendar/CalendarDBService.py
+++ academics/calendar/CalendarDBService.py
@@ -1,6 +1,7 @@
 import boto3
 from boto3.dynamodb.conditions import Key,Attr
 import academics.calendar.Calendar as cal
+from academics.logger import GCLogger as logger
 
 
 CALENDAR_TBL = 'Calendar'
@@ -16,21 +17,32 @@ def delete_calendar(calendar_key) :
     )
     return response
 
-# TODO Change the implementation
-# a new index of institution_key and subscriber_type is added
-# Also implement getting attitional items if in one query all items are not returned
-#
-def get_all_calendars(institution_key,subscriber_type):
-    calendar_list =[]
+
+def get_all_calendars(institution_key, subscriber_type):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(CALENDAR_TBL)
-    response = table.scan(
-        FilterExpression=Attr("institution_key").eq(institution_key) & Attr('subscriber_type').eq(subscriber_type)
+    calendars = []
+    response = table.query(
+        IndexName='institution_key-subscriber_type-index',
+        KeyConditionExpression=Key('institution_key').eq(institution_key) & Key('subscriber_type').eq(subscriber_type)
     )
-    for item in response['Items'] :
+    add_calendars_from_response(response, calendars)
+    logger.debug('Intermediary calendars count - ' + str(len(calendars)))
+    while 'LastEvaluatedKey' in response:
+        response = table.query(
+            IndexName='institution_key-subscriber_type-index',
+           KeyConditionExpression=Key('institution_key').eq(institution_key) & Key('subscriber_type').eq(subscriber_type)
+        )
+        add_calendars_from_response(response, calendars)
+        logger.debug('Intermediary calendars count - ' + str(len(calendars)))
+    return calendars
+
+
+def add_calendars_from_response(response, calendars):
+    for item in response['Items']:
         calendar = cal.Calendar(item)
-        calendar_list.append(calendar)
-    return calendar_list
+        calendars.append(calendar)
+
 
 
 def add_or_update_calendar(calendar):
